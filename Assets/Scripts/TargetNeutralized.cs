50f0cb07da4d30721a02235be2611443b5d8a0a4
Chuck <charles.karim00@gmail.com>
2021-02-24 13:35:34 -0500

    Tried adding a score
    Score gets reset since the script is found in a prefab

--- Assets/Scripts/TargetNeutralized.cs
+++ Assets/Scripts/TargetNeutralized.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetNeutralized : MonoBehaviour
 {
     private AttackScript attack;
     private HealthScript health;
+    private Text scoreText;
+    int score;
 
     private void Awake()
     {
         attack = GameObject.Find("AttackTarget").GetComponent<AttackScript>();
         GameObject go = GameObject.Find("/FirstPersonPlayer/MainCamera");
-        Debug.Log(go.name);
-
         health = go.GetComponent<HealthScript>();
+        scoreText = GameObject.Find("/PlayerCanvas/Score").GetComponent<Text>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,12 +26,26 @@ public class TargetNeutralized : MonoBehaviour
         if (collObj.name == "Fist" && attack.punching)
         {
             GetComponent<Rigidbody>().AddForce(punchForce * 1000);
+            score += 10;
         }
         else if (collObj.name == "Cylinder")
         {
             Debug.Log("Ouch!");
             health.TakeDamage(500f);
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 10;
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "SCORE: " + score.ToString();
     }
 
 }
